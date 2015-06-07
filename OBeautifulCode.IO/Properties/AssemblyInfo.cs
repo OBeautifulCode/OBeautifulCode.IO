14da2d99676a3849ad4d03f96fbf21b4592d58a8
Suraj Gupta <surajg@gmail.com>
2015-06-06 21:58:28 -0400

    make CLSCompliant

--- OBeautifulCode.IO/Properties/AssemblyInfo.cs
+++ OBeautifulCode.IO/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -44,3 +45,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("OBeautifulCode.IO.Test")]
+[assembly: CLSCompliant(true)]
