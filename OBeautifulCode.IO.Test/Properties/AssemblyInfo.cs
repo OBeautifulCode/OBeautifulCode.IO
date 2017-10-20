ca42905feb98d595e5e37a6831f0251af581a371
Suraj Gupta <surajg@gmail.com>
2017-10-20 13:34:47 -0400

    fix failing unit tests
    don't run in parallel

--- OBeautifulCode.IO.Test/Properties/AssemblyInfo.cs
+++ OBeautifulCode.IO.Test/Properties/AssemblyInfo.cs
@@ -8,6 +8,8 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+using Xunit;
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -41,3 +43,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
