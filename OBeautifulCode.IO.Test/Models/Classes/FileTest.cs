8b86587d704b20a1c9a8968a5798949834b5e5dd
Suraj Gupta <SurajGupta@users.noreply.github.com>
2023-05-04 10:56:21 -0400

    added File class

---
+++ OBeautifulCode.IO.Test/Models/Classes/FileTest.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FileTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.IO.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    using FakeItEasy;
+
+    using OBeautifulCode.AutoFakeItEasy;
+    using OBeautifulCode.CodeAnalysis.Recipes;
+    using OBeautifulCode.CodeGen.ModelObject.Recipes;
+    using OBeautifulCode.Math.Recipes;
+
+    using Xunit;
+
+    using static System.FormattableString;
+
+    using File = OBeautifulCode.IO.File;
+
+    [SuppressMessage("Microsoft.Maintainability", "CA1505:AvoidUnmaintainableCode", Justification = ObcSuppressBecause.CA1505_AvoidUnmaintainableCode_DisagreeWithAssessment)]
+    public static partial class FileTest
+    {
+        [SuppressMessage("Microsoft.Maintainability", "CA1505:AvoidUnmaintainableCode", Justification = ObcSuppressBecause.CA1505_AvoidUnmaintainableCode_DisagreeWithAssessment)]
+        [SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Justification = ObcSuppressBecause.CA1810_InitializeReferenceTypeStaticFieldsInline_FieldsDeclaredInCodeGeneratedPartialTestClass)]
+        static FileTest()
+        {
+            ConstructorArgumentValidationTestScenarios
+                .RemoveAllScenarios()
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<File>
+                    {
+                        Name = "constructor should throw ArgumentNullException when parameter 'bytes' is null scenario",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<File>();
+
+                            var result = new File(
+                                                 null,
+                                                 referenceObject.FileName,
+                                                 referenceObject.FileFormat);
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentNullException),
+                        ExpectedExceptionMessageContains = new[] { "bytes", },
+                    });
+        }
+    }
+}
---
+++ OBeautifulCode.IO/Models/Classes/File.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="File.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.IO
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using OBeautifulCode.CodeAnalysis.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Represents a file.
+    /// </summary>
+    public partial class File : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="File"/> class.
+        /// </summary>
+        /// <param name="bytes">The bytes of the file.</param>
+        /// <param name="fileName">OPTIONAL name of the file.  DEFAULT is to an unspecified name.</param>
+        /// <param name="fileFormat">OPTIONAL format of the file.  DEFAULT is unspecified.</param>
+        [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "bytes", Justification = ObcSuppressBecause.CA1720_IdentifiersShouldNotContainTypeNames_TypeNameAddsClarityToIdentifierAndAlternativesDegradeClarity)]
+        public File(
+            byte[] bytes,
+            string fileName = null,
+            FileFormat fileFormat = FileFormat.Unspecified)
+        {
+            if (bytes == null)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+
+            this.Bytes = bytes;
+            this.FileName = fileName;
+            this.FileFormat = fileFormat;
+        }
+
+        /// <summary>
+        /// Gets the bytes of the file.
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays", Justification = ObcSuppressBecause.CA1819_PropertiesShouldNotReturnArrays_DataPayloadsAreCommonlyRepresentedAsByteArrays)]
+        public byte[] Bytes { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the file.
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Gets the format of the file.
+        /// </summary>
+        public FileFormat FileFormat { get; private set; }
+    }
+}
