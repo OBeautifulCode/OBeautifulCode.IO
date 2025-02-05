d9b059690c2e5b0e08372ebe736747f429f39f3c
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-02-05 15:56:14 -0500

    added TagNames constants

---
+++ OBeautifulCode.IO/Logic/TagNames.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TagNames.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.IO
+{
+    /// <summary>
+    /// Tag name constants.
+    /// </summary>
+    public static class TagNames
+    {
+        /// <summary>
+        /// The tag name for <see cref="IO.MediaType"/>.
+        /// </summary>
+        /// <remarks>
+        /// Note that this is purposefully not content-type so that it doesn't conflict the content-type
+        /// header in API calls.
+        /// </remarks>
+        public const string MediaType = "media-type";
+
+        /// <summary>
+        /// The tag name for a malware scan result.
+        /// </summary>
+        public const string MalwareScanResult = "malware-scan-result";
+    }
+}
