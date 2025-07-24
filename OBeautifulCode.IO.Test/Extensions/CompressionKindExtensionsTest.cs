34b18379e8e4127c9c8f7e9cc7b10f198ed0b911
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-07-24 15:56:31 -0400

    added CompresionKind.ToMediaType extension

---
+++ OBeautifulCode.IO.Test/Extensions/CompressionKindExtensionsTest.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompressionKindExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.IO.Test
+{
+    using System;
+    using System.Linq;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Compression;
+    using Xunit;
+
+    public static class CompressionKindExtensionsTest
+    {
+        [Fact]
+        public static void ToMediaType___Should_throw_ArgumentOutOfRangeException___When_parameter_compressionKind_is_Invalid()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => CompressionKind.Invalid.ToMediaType());
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentOutOfRangeException>();
+            actual.Message.AsTest().Must().ContainString("compressionKind");
+            actual.Message.AsTest().Must().ContainString("Invalid");
+        }
+
+        [Fact]
+        public static void ToMediaType___Should_return_null___When_parameter_compressionKind_is_None()
+        {
+            // Arrange, Act
+            var actual = CompressionKind.None.ToMediaType();
+
+            // Assert
+            actual.AsTest().Must().BeNull();
+        }
+
+        [Fact]
+        public static void ToMediaType___Should_return_MediaType_for_all_CompressionKind_values___When_called()
+        {
+            // Arrange
+            var compressionKindAndExpected = new[]
+            {
+                new { CompressionKind = CompressionKind.DotNetZip, Expected = (MediaType?)MediaType.ApplicationGzip },
+            };
+
+            var expected = compressionKindAndExpected.Select(_ => _.Expected).ToList();
+
+            // Act
+            var actual = compressionKindAndExpected.Select(_ => _.CompressionKind.ToMediaType()).ToList();
+
+            // Assert
+            actual.AsTest().Must().BeEqualTo(expected);
+        }
+    }
+}
---
+++ OBeautifulCode.IO/Logic/CompressionKindExtensions.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompressionKindExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.IO
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using OBeautifulCode.CodeAnalysis.Recipes;
+    using OBeautifulCode.Compression;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Extension methods on <see cref="CompressionKindExtensions"/>.
+    /// </summary>
+    public static class CompressionKindExtensions
+    {
+        /// <summary>
+        /// Gets the media type for a specified compression kind.
+        /// </summary>
+        /// <param name="compressionKind">The compression kind.</param>
+        /// <returns>
+        /// Returns null if <paramref name="compressionKind"/> is <see cref="CompressionKind.None"/>,
+        /// otherwise returns the corresponding media type.
+        /// </returns>
+        [SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity", Justification = ObcSuppressBecause.CA1502_AvoidExcessiveComplexity_DisagreeWithAssessment)]
+        public static MediaType? ToMediaType(
+            this CompressionKind compressionKind)
+        {
+            MediaType? result;
+
+            if (compressionKind == CompressionKind.Invalid)
+            {
+                throw new ArgumentOutOfRangeException(Invariant($"{nameof(compressionKind)} is {CompressionKind.Invalid}."));
+            }
+            else if (compressionKind == CompressionKind.None)
+            {
+                result = null;
+            }
+            else if (compressionKind == CompressionKind.DotNetZip)
+            {
+                result = MediaType.ApplicationGzip;
+            }
+            else
+            {
+                throw new NotSupportedException(Invariant($"This {nameof(CompressionKind)} is not supported: {compressionKind}."));
+            }
+
+            return result;
+        }
+    }
+}
